e9e6831e0b71ef7ddf36a6c9b78a10bc292ebfb8
stoyat <evgenii_r84@mail.ru>
2017-03-26 13:12:43 +0300

    add migration order and articles

---
+++ database/migrations/2017_03_22_184036_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+
+            $table->increments('id');
+            $table->string('name');
+            $table->string('surname');
+            $table->text('address');
+            $table->string('phone');
+            $table->text('cart');
+            $table->string('total');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
